9d5b01b5d19e9d55ccde6b574202d3806de54911
jsjasinski <jj.jakub.jasinski@gmail.com>
2022-04-10 16:33:11 +0200

    BufferedMutableSharedFlow replay parameter change

--- app/src/main/java/com/jj/sensorcollector/framework/utils/KotlinFlowUtils.kt
+++ app/src/main/java/com/jj/sensorcollector/framework/utils/KotlinFlowUtils.kt
@@ -4,6 +4,6 @@ import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 @Suppress("FunctionName")
-fun <T> BufferedMutableSharedFlow(replay: Int = 0, extraBufferCapacity: Int = 1,
+fun <T> BufferedMutableSharedFlow(replay: Int = 1, extraBufferCapacity: Int = 1,
                                   onBufferOverflow: BufferOverflow = BufferOverflow.DROP_OLDEST): MutableSharedFlow<T> =
     MutableSharedFlow(replay, extraBufferCapacity, onBufferOverflow)
