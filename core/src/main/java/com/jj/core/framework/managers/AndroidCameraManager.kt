5bc87d5cb6df0e573ac21300e2a46a0baa10b792
jsjasinski <jj.jakub.jasinski@gmail.com>
2022-06-28 23:59:06 +0200

    works!

--- core/src/main/java/com/jj/core/framework/managers/AndroidCameraManager.kt
+++ core/src/main/java/com/jj/core/framework/managers/AndroidCameraManager.kt
@@ -6,26 +6,20 @@ import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import com.google.common.util.concurrent.ListenableFuture
 import com.jj.core.domain.managers.CameraManager
 import java.io.File
 
 class AndroidCameraManager(
-    context: Context
+    private val context: Context
 ) : CameraManager {
 
-    private val cameraFuture: ListenableFuture<ProcessCameraProvider> = ProcessCameraProvider.getInstance(context)
     private val imageCapture = ImageCapture.Builder().build()
     private val executor = ContextCompat.getMainExecutor(context)
-    private val preview = Preview.Builder().build()
-    private val previewView = PreviewView(context)
     private val selector = CameraSelector.Builder()
         .requireLensFacing(CameraSelector.LENS_FACING_BACK)
         .build()
@@ -48,16 +42,6 @@ class AndroidCameraManager(
         }
     }
 
-    init {
-        preview.setSurfaceProvider(previewView.surfaceProvider)
-        cameraFuture.get().bindToLifecycle(
-            lifecycleOwner,
-            selector,
-            imageCapture,
-            preview
-        )
-    }
-
     private val callback = object : ImageCapture.OnImageCapturedCallback() {
         override fun onCaptureSuccess(image: ImageProxy) {
             super.onCaptureSuccess(image)
@@ -71,7 +55,15 @@ class AndroidCameraManager(
         }
     }
 
+    // 1. Get instance before someone else takes it, i.e. view
+    // 2. Don't pass preview if it is not used
+    // TODO Try to return preview from here to other views? just to have getInstance in one place called once
     override fun takePhoto() {
+        ProcessCameraProvider.getInstance(context).get().bindToLifecycle(
+            lifecycleOwner,
+            selector,
+            imageCapture
+        )
         val outputFileOptions = ImageCapture.OutputFileOptions.Builder(File("aa")).build()
         imageCapture.takePicture(executor, callback)
     }
