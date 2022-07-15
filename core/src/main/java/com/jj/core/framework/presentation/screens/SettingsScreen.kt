b80266bfe77fdf95d5fcbad53dbc301a0105effc
jsjasinski <jj.jakub.jasinski@gmail.com>
2022-07-15 11:04:40 +0200

    Adjust top bar on SettingsScreen

--- core/src/main/java/com/jj/core/framework/presentation/screens/SettingsScreen.kt
+++ core/src/main/java/com/jj/core/framework/presentation/screens/SettingsScreen.kt
@@ -14,10 +14,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jj.core.R
 import com.jj.core.framework.presentation.viewmodels.SettingsScreenViewModel
 import com.jj.design.CameraPreview
+import com.jj.design.components.BaseContainer
 import org.koin.androidx.compose.getViewModel
 
 @Composable
@@ -32,10 +35,8 @@ fun SettingsScreen(
 fun SettingsScreenContent(
     registerCameraPreview: (androidx.camera.core.Preview) -> Unit,
 ) {
-    Scaffold(
-        topBar = {
-            Text("Settings")
-        }
+    BaseContainer(
+        appBarTitle = stringResource(R.string.settings)
     ) {
         Column(
             modifier = Modifier
