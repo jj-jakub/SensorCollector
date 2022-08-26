f08c3b519db9977040cfe74cac05c8577f8c965c
jsjasinski <jj.jakub.jasinski@gmail.com>
2022-08-26 18:38:49 +0200

    Initialize app in MainActivity

--- app/src/main/java/com/jj/sensorcollector/presentation/SensorCollectorApplication.kt
+++ app/src/main/java/com/jj/sensorcollector/presentation/SensorCollectorApplication.kt
@@ -1,22 +1,14 @@
 package com.jj.sensorcollector.presentation
 
 import android.app.Application
-import com.jj.domain.base.initializer.ProgramInitializer
 import com.jj.sensorcollector.di.KoinLauncher
-import kotlinx.coroutines.runBlocking
-import org.koin.android.ext.android.inject
 
 class SensorCollectorApplication : Application() {
 
     private val koinLauncher: KoinLauncher = KoinLauncher()
 
-    private val programInitializer: ProgramInitializer by inject()
-
     override fun onCreate() {
         super.onCreate()
         koinLauncher.startKoin(this)
-        runBlocking {
-            programInitializer.initialize()
-        }
     }
 }
--- app/src/main/java/com/jj/sensorcollector/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/jj/sensorcollector/presentation/activities/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import com.jj.core.framework.presentation.navigation.MainViewRoot
+import com.jj.domain.base.initializer.ProgramInitializer
+import kotlinx.coroutines.runBlocking
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private val programInitializer: ProgramInitializer by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MainViewRoot()
+        }
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        runBlocking {
+            programInitializer.initialize()
         }
     }
 }
