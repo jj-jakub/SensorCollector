36621b5f0858aabb66140cf0bf0ad5161ce2997d
jsjasinski <jj.jakub.jasinski@gmail.com>
2022-08-27 11:53:08 +0200

    PathAnalyser NaN crash fix

--- core/src/main/java/com/jj/core/data/hardware/gps/analysis/DefaultGPSPathAnalyser.kt
+++ core/src/main/java/com/jj/core/data/hardware/gps/analysis/DefaultGPSPathAnalyser.kt
@@ -2,11 +2,11 @@ package com.jj.core.data.hardware.gps.analysis
 
 import android.util.Log
 import com.jj.domain.coroutines.CoroutineScopeProvider
+import com.jj.domain.hardware.gps.analysis.GPSPathAnalyser
+import com.jj.domain.hardware.gps.analysis.GPSVelocityCalculator
 import com.jj.domain.hardware.gps.repository.GPSRepository
 import com.jj.domain.hardware.gps.repository.PathRepository
 import com.jj.domain.model.analysis.AnalysedSample
-import com.jj.domain.hardware.gps.analysis.GPSPathAnalyser
-import com.jj.domain.hardware.gps.analysis.GPSVelocityCalculator
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
@@ -34,7 +34,10 @@ class DefaultGPSPathAnalyser(
     private fun onAnalysedSampleAvailable(analysedGPSSample: AnalysedSample.AnalysedGPSSample) {
         previousSample?.let { prevSample ->
             val velocity = gpsVelocityCalculator.calculateCurrentVelocity(prevSample, analysedGPSSample)
-            val roundedVelocity = ((velocity * 100).roundToInt()) / 100
+
+            val roundedVelocity = ((if (velocity.isNaN()) 0.0
+            else velocity
+                * 100).roundToInt()) / 100
             Log.d("ABABV", "Current velocity: $roundedVelocity")
         } ?: run {
             Log.d("ABABV", "Waiting for second sample")
